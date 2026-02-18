65e3f16421f1ecd71d7906647bf1b75ae7373e2d
Administrator <2776914626@qq.com>
2026-02-18 18:50:09 +0800

    包

---
+++ src/come/test/Main.java
@@ -0,0 +1,20 @@
+package come.test;
+
+public class Main {
+    public static  void main(){
+        //通过new来创建对象,再通过同类变量接收
+        Person p1=null;//这里表示空，无法操作p1
+       Person p= new Person("小明",18);
+       Person q= new Person("小明",18);
+        //对象的使用
+        p.age=11;
+        p.name="肖梦";
+        p.sex="妞";
+        p.hello();
+       int s= p.Sum(3,4);
+       System.out.println(s);
+       q.hello();
+    }
+}
+//包的声名与导入，将类放到包里，不在一个包里的类要进行导入才可以使用通过import关键字导入
+//不同包里的类即使类名相同也是不同类,但是同名类引用到一个包里要手动补全包名
---
+++ src/come/test/Person.java
@@ -0,0 +1,24 @@
+package come.test;
+
+public class Person {
+    //类的定义与对象创建
+    String name;
+    int age;
+    String sex;
+    static String info;//静态变量，所有对象共用的
+    //方法的创建与使用，相当于c语言中的函数
+   public void hello(){
+        System.out.println("我叫"+name+"今年"+age+"岁了");
+    }
+    //让人类做加法运算
+   public int Sum(int a,int b){
+        return a+b;
+    }
+ public Person(String name,int age){
+        //构造方法,方法名与类名相同，相当于c++中的构造函数,当一个类中有多个构造方法时，可以用this()调用想要的方法，但是只可以调用一次
+    this.name=name;//可以进行初始化
+    this.age=age;
+}
+public static void test(){}
+    //静态方法不可一访问对象特有的东西，但可以访问静态变量与c++差不多
+}
